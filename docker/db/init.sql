1be3c6e0a85cc68509949d431ffbee03e04c61f1
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-07-18 22:55:14 +0300

    Updated docker config

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,3 +1,3 @@
--- CREATE USER pavel;
--- CREATE DATABASE teacher_selector;
--- GRANT ALL PRIVILEGES ON DATABASE teacher_selector TO pavel;
+CREATE USER pavel;
+CREATE DATABASE teacher_selector;
+GRANT ALL PRIVILEGES ON DATABASE teacher_selector TO pavel;
