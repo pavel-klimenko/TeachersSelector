38f36ca2426f2c4c7adf3aba0e560cb1b832be4d
Pavel-Klimenko <pavel.klimenko.1989@gmail.com>
2026-02-10 22:15:10 +0300

    Deleted unused class

--- PersonalChatBundle/src/Infrastructure/Message/RabbitMessageHandler.php
+++ PersonalChatBundle/src/Infrastructure/Message/RabbitMessageHandler.php
@@ -5,10 +5,8 @@ namespace PersonalChatBundle\Infrastructure\Message;
 
 use PersonalChatBundle\Application\Chats\PersonalChat\Symfony\Message\SendChatMessage;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use PersonalChatBundle\Domain\Entity\PersonalChatMessage;
 use PersonalChatBundle\Domain\Repository\ChatParticipantRepositoryInterface;
 use PersonalChatBundle\Domain\Repository\PersonalChatRepositoryInterface;
-use PersonalChatBundle\Domain\Repository\PersonalChatMessagesRepositoryInterface;
 use PersonalChatBundle\Domain\ValueObject\PersonalChatMessage\Message as MessageVO;
 
 #[AsMessageHandler]
@@ -17,7 +15,6 @@ final class RabbitMessageHandler
     public function __construct(
         private PersonalChatRepositoryInterface $personalChatRepository,
         private ChatParticipantRepositoryInterface $chatParticipantRepository,
-        private PersonalChatMessagesRepositoryInterface $personalChatMessageRepository,
     ) {}
 
     public function __invoke(SendChatMessage $dto): void
@@ -27,8 +24,5 @@ final class RabbitMessageHandler
 
         $personalChat->addMessage($chatParticipant, new MessageVO($dto->message));
         $this->personalChatRepository->save($personalChat);
-
-        //$newMessage = new PersonalChatMessage($personalChat, $chatParticipant, $newMessage)
-
     }
 }
